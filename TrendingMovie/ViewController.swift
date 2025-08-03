5303905f421ebb3eab23dcb91be7897e36e6e247
Arqam Owais <arqam.owais@veripark.com>
2025-08-03 18:18:59 +0500

    Initial commit

---
+++ TrendingMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendingMovie
+//
+//  Created by Arqam Owais on 03/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
